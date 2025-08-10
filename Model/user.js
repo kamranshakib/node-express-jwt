123f1ae2275aa50fcd9fece0a560ac10c52cbd72
Kamran Shakib <kamranshakib371@gmail.com>
2025-08-10 09:23:03 -0700

    delete database code form app.js and move to model folder

---
+++ Model/user.js
@@ -0,0 +1,25 @@
+import mongoose, { mongo } from "mongoose";
+
+const DBURL = 'mongodb+srv://kamranshakib371:ZC7e8K4dAHxwHgE@cluster0.zztg2xz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(DBURL)
+.then(()=> console.log('Connected to Database'))
+.catch((err)=> console.log(err.message))
+
+const userSchema = new mongoose.Schema({
+
+
+    email: {
+        required: [true, 'please enter your email'],
+        unique: [true, 'enter another email address']
+    },
+    password: {
+        required: [true, 'please enter your password.'],
+        minLength: [8,'your passwoed is small then 8 char..'],
+    }
+
+
+})
+
+const User = mongoose.model('user-auth',userSchema);
+export default User;
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
+
+import User from'./Model/user.js'
 
 const app = express();
 
@@ -9,11 +10,6 @@ app.use(express.static('public'));
 // view engine
 app.set('view engine', 'ejs');
 
-// database connection
-// const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-//   .then((result) => console.log('connected to DB'))
-//   .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
