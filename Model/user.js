9265ac2e98e9ccf88ad9d848137128c0c5a39c4e
Kamran Shakib <kamranshakib371@gmail.com>
2025-08-10 09:26:06 -0700

    change required to import

--- Model/user.js
+++ Model/user.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const DBURL = 'mongodb+srv://kamranshakib371:ZC7e8K4dAHxwHgE@cluster0.zztg2xz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
@@ -7,15 +7,15 @@ mongoose.connect(DBURL)
 .catch((err)=> console.log(err.message))
 
 const userSchema = new mongoose.Schema({
-
-
     email: {
+        type: String,
         required: [true, 'please enter your email'],
         unique: [true, 'enter another email address']
     },
     password: {
+        type: String,
         required: [true, 'please enter your password.'],
-        minLength: [8,'your passwoed is small then 8 char..'],
+        minlength: [8,'your passwoed is small then 8 char..'],
     }
 
 
