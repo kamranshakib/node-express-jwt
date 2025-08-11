2a8e35e92406bdfa3d8b29a841aac2e904fb5676
Kamran Shakib <kamranshakib371@gmail.com>
2025-08-11 11:34:26 -0700

    make routes for login and signup

---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const router = express.Router();
+import * as authControllers  from '../controllers/authControllers.js'
+
+
+router.get('/signup',authControllers.signup)
+router.post('/signup',authControllers.signup_post)
+router.get('/login',authControllers.login)
+router.post('/login',authControllers.login_post)
+
+
+export default router;
