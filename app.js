52145392f8c6d264730fcfa8b62081578cad533b
Kamran Shakib <kamranshakib371@gmail.com>
2025-08-19 09:16:16 -0700

    set and git cookies

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-import express from 'express'
+import express from "express";
+import cookieParser from "cookie-parser";
 
-import User from'./Model/user.js'
-import authRoutes from './routes/authRoutes.js'
+import User from "./Model/user.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(express.json())
-app.use(authRoutes)
+app.use(express.json());
+app.use(cookieParser())
+app.use(authRoutes);
 // view engine
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
 
 // routes
-app.get('/', (req, res) => res.render('home'));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000,()=>{
-  console.log('localhost 3000')
+
+
+app.get('/set-cookies',(req,res)=>{
+
+  res.cookie('newUser',true,{maxAge: 100*60*60,httpOnly:true})
+  res.cookie('isEmployee',true)
+  res.send('get cookies')
 })
+
+app.get('/get-cookie',(req,res)=>{
+  const cookies = req.cookies;
+  res.json(cookies)
+})
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("localhost 3000");
+});
--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,51 +1,45 @@
+import User from "../Model/user.js";
 
-import User from "../Model/user.js"
+const ErrorHundle = (err) => {
+  console.log(err.message, err.code);
 
+  let errors = { email: "", password: "" };
 
-
-const ErrorHundle = (err)=>{
-    console.log(err.message, err.code)
-
-    let errors = {email:'',password: ''};
-    
-    //duplicate errors code
-    if(err.code ===11000){
-        errors.email = 'that email is aleady registered'
-        return errors;
-    }
-
-    if(err.message.includes('user-auth validation failed')){
-       Object.values(err.errors).forEach(({properties})=>{
-        errors[properties.path]= properties.message
-       })
-
-    }
+  //duplicate errors code
+  if (err.code === 11000) {
+    errors.email = "that email is aleady registered";
     return errors;
-}
+  }
 
- export const login = (req,res)=>{
-    res.render('login')
-}
+  if (err.message.includes("user-auth validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
 
-export const signup = (req,res)=>{
-    res.render('signup')
-}
+export const login = (req, res) => {
+  res.render("login");
+};
 
-export const login_post = (req,res)=>{
-    res.send('new login')
-}
+export const signup = (req, res) => {
+  res.render("signup");
+};
 
- export const signup_post = async (req,res)=>{
-    try {
-        const {email, password} = await req.body;
-        const newUser = await User.create({email,password});
-        res.status(200).json({
-            user: newUser
-        })
-        
-    } catch (err) {
-      const errors =  ErrorHundle(err);
-        res.status(500).json({errors})
-        
-    }
-}
+export const login_post = (req, res) => {
+  res.send("new login");
+};
+
+export const signup_post = async (req, res) => {
+  try {
+    const { email, password } = await req.body;
+    const newUser = await User.create({ email, password });
+    res.status(200).json({
+      user: newUser,
+    });
+  } catch (err) {
+    const errors = ErrorHundle(err);
+    res.status(500).json({ errors });
+  }
+};
